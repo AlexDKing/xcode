dacfb91515d0e447934abbbcbf2d4659f4e7069d
Alex king <1017527036@qq.com>
2022-07-08 00:44:09 +0800

    Initial Commit

---
+++ yes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  yes
+//
+//  Created by Alex·King on 2022/7/8.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
